3a8ba5beae8850dc43b19ed1e19450d5aa6d4dc4
Sooyca <281437@gmail.com>
2018-09-01 23:33:42 +0200

    a

--- compile_tex.sh
+++ compile_tex.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-pdflatex praca
-sleep 0.5s
-bibtex praca
-sleep 0.5s
-pdflatex praca
-sleep 0.5s
-pdflatex praca
+pdflatex -synctex=1 -interaction=nonstopmode -file-line-error praca &&
+#sleep 0.5s
+bibtex praca &&
+#sleep 0.5s
+pdflatex -synctex=1 -interaction=nonstopmode -file-line-error praca &&
+#sleep 0.5s
+pdflatex -synctex=1 -interaction=nonstopmode -file-line-error praca
 
